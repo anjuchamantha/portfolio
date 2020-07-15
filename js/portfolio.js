7530af63dd02f79f703c3b0f4fe06d7f6b581c1f
Anju Chamantha <chamantha97anju@gmail.com>
2020-07-15 18:05:36 +0530

    init js

---
+++ js/portfolio.js
@@ -0,0 +1,19 @@
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'data/achievements.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+loadJSON(function (response) {
+    // Parse JSON string into object
+    var actual_JSON = JSON.parse(response);
+    console.log(actual_JSON);
+    document.getElementById('education').innerHTML = actual_JSON[0].name;
+});
