9ca10ad53f07a0b9a0ab3a2418245beefd503bec
Anju Chamantha <chamantha97anju@gmail.com>
2020-07-17 14:46:53 +0530

    education tab

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,47 +1,3 @@
-function loadJSON(filepath, callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', filepath, true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
-
-function makeItem(item) {
-    let place = "<span title=\"Placement\">" + "<i class=\"fa fa-trophy\" aria-hidden=\"true\"></i>" + item.place + "</span>"
-    let date = "<span title=\"Date\">" + "<i class=\"fa fa-calendar\" aria-hidden=\"true\"></i>" + item.date + "</span>"
-    let organizer = "<span title=\"Organizer\">" + "<i class=\"fa fa-globe\" aria-hidden=\"true\"></i>" + item.organizer + "</span>"
-    let name = "<h3>" + item.name + place + date + organizer + "</h3>"
-    let detail = "<div class=\"overlay\">" + item.detail + "</div>"
-    let img = "<a href=\"" + item.img + "\">" +
-        "<div class=\"item-img\" >" +
-        "<img class=\"image\" src=\"" + item.img + "\">" +
-        detail +
-        "</div ></a>"
-    let section = "<section class=\"item\">" + name + img + "</section>"
-    return section;
-}
-
-function displaySection(items) {
-    let sec = ""
-    for (let i = 0; i < items.length; i++) {
-        sec += makeItem(items[i]);
-    }
-    document.getElementById('achievements').innerHTML = sec;
-}
-
-loadJSON('data/achievements.json', function (response) {
-    // Parse JSON string into object
-    var achievements = JSON.parse(response);
-    displaySection(achievements)
-
-
-});
-
 // TAB NAV
 
 let tabButtons = document.querySelectorAll(".tabContainer .buttonContainer button");
@@ -63,5 +19,76 @@ function showPanel(panelIndex) {
     tabPanels[panelIndex].style.display = "block";
 }
 
+function loadJSON(filepath, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', filepath, true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function makeAchievementItem(item) {
+    let place = "<span title=\"Placement\">" + "<i class=\"fa fa-trophy\" aria-hidden=\"true\"></i>" + item.place + "</span>"
+    let date = "<span title=\"Date\">" + "<i class=\"fa fa-calendar\" aria-hidden=\"true\"></i>" + item.date + "</span>"
+    let organizer = "<span title=\"Organizer\">" + "<i class=\"fa fa-globe\" aria-hidden=\"true\"></i>" + item.organizer + "</span>"
+    let name = "<h3>" + item.name + place + date + organizer + "</h3>"
+    let detail = "<div class=\"overlay\">" + "item.detail" + "</div>"
+    let img = "<a href=\"" + item.img + "\">" +
+        "<div class=\"item-img\" >" +
+        "<img class=\"image\" src=\"" + item.img + "\">" +
+        detail +
+        "</div ></a>"
+    let section = "<section class=\"item\">" + name + img + "</section>"
+    return section;
+}
+function makeEducationItem(item) {
+    let date = "<span title=\"Date\">" + "<i class=\"fa fa-calendar\" aria-hidden=\"true\"></i>" + item.date + "</span>"
+    let name = "<h3>" + item.institute + date + "</h3>"
+    let programs = "";
+    for (let i = 0; i < item.programs.length; i++) {
+        const program = item.programs[i];
+        let program_li = "<li>" + program + "<span class=\"item-li-span\">" + item.grades[i] + "</span>" + "</li>";
+        programs += program_li;
+    }
+    let programs_div = "<div class=\"item-detail\"><ul>" + programs + "</ul></div>";
+    let section = "<section class=\"item\">" + name + programs_div + "</section>";
+    return section;
+}
+
+function displaySection(items, element) {
+    let sec = ""
+    for (let i = 0; i < items.length; i++) {
+        switch (element) {
+            case "achievements":
+                sec += makeAchievementItem(items[i]);
+                break;
+            case "education":
+                sec += makeEducationItem(items[i]);
+                break;
+        }
+    }
+    document.getElementById(element).innerHTML = sec;
+}
+
+
+
+
 // show first panel when page loads
 showPanel(0);
+
+loadJSON('data/achievements.json', function (response) {
+    // Parse JSON string into object
+    var items = JSON.parse(response);
+    displaySection(items, "achievements")
+});
+
+loadJSON('data/education.json', function (response) {
+    // Parse JSON string into object
+    var items = JSON.parse(response);
+    displaySection(items, "education")
+});
